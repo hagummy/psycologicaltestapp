124c9e0681b49078a67cc8ca064625923eed4f92
hagummy <haky619@naver.com>
2022-04-19 02:30:11 +0900

    first commit

--- app/src/main/java/com/example/psycologicaltestapp/MainActivity.kt
+++ app/src/main/java/com/example/psycologicaltestapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.psycologicaltestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
     }
 }
---
+++ app/src/main/java/com/example/psycologicaltestapp/fragment/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.example.psycologicaltestapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.psycologicaltestapp.R
+
+
+class MainFragment : Fragment() {
+
+
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+
+        navController= Navigation.findNavController(view)
+
+    }
+
+
+
+}
